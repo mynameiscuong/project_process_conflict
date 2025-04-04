5537c8aa970ee5538268d5af91fc52837bd65fac
mynameiscuong <ngoccuong147512@gmail.com>
2025-04-04 10:31:58 +0700

    cuong conflict lần 2

--- components/Footer.js
+++ components/Footer.js
@@ -7,6 +7,8 @@ function Footer() {
       <h2>Test</h2>
       <h3>Lần 2</h3>
       <div>thuộc nhánh cuong</div>
+      <div>thuộc nhánh cuong_1</div>
+      <div>thuộc nhánh cuong lần 2</div>
     </>
   );
 }
